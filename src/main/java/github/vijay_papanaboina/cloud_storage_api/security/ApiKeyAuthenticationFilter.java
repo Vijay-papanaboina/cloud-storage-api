4aaf47abd8011474e524c8c26d6f385722467652
vijay-papanaboina <vijaypapanaboina3@gmail.com>
2025-11-11 16:21:04 +0530

    Update ApiKeyAuthenticationFilter to use lazy initialization for dependencies
    - Modify constructor of ApiKeyAuthenticationFilter to use @Lazy annotation for ApiKeyRepository and ApiKeyService, improving performance by deferring initialization until needed.

--- src/main/java/github/vijay_papanaboina/cloud_storage_api/security/ApiKeyAuthenticationFilter.java
+++ src/main/java/github/vijay_papanaboina/cloud_storage_api/security/ApiKeyAuthenticationFilter.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.Authentication;
@@ -42,7 +43,7 @@ public class ApiKeyAuthenticationFilter extends OncePerRequestFilter {
     private String apiKeyHeaderName;
 
     @Autowired
-    public ApiKeyAuthenticationFilter(ApiKeyRepository apiKeyRepository, ApiKeyService apiKeyService) {
+    public ApiKeyAuthenticationFilter(@Lazy ApiKeyRepository apiKeyRepository, @Lazy ApiKeyService apiKeyService) {
         this.apiKeyRepository = apiKeyRepository;
         this.apiKeyService = apiKeyService;
     }
